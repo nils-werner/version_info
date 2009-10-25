0fd3421c4c32b2ba702eecc93111ecc3629e8cae
Nils Werner <nils.werner@gmail.com>
2009-10-25 17:52:31 +0100

    Initial version

---
+++ README.txt
@@ -0,0 +1,23 @@
+
+Version Info
+------------------------------------
+
+Version: 1.0
+Author: Nils Werner (nils.werner@gmail.com)
+Build Date: 2009-10-25
+Requirements: Symphony 2.0
+
+
+[INSTALLATION]
+
+1. Upload the 'version_info' folder in this archive to your Symphony 'extensions' folder.
+
+2. Enable it by selecting the "Version Info" entry, choose Enable from the with-selected menu, then click Apply.
+
+3. You will now see your Symphony version number in the footer
+
+
+[CHANGELOG]
+
+1.0
+- Initial release
---
+++ extension.driver.php
@@ -0,0 +1,38 @@
+<?php
+
+error_reporting(E_ALL);
+
+	Class extension_version_info extends Extension{
+	
+		public function about(){
+			return array('name' => 'Version Info',
+						 'version' => '1.0',
+						 'release-date' => '2009-10-25',
+						 'author' => array('name' => 'Nils Werner',
+										   'website' => 'http://www.phoque.com/projekte/symphony',
+										   'email' => 'nils.werner@gmail.com')
+				 		);
+		}
+		
+		
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page'		=> '/backend/',
+					'delegate'	=> 'AddElementToFooter',
+					'callback'	=> 'AddElementToFooter'
+				)
+			);
+		}
+
+		public function AddElementToFooter($context) {
+			$ul = &$context['wrapper'];
+			
+			$li = new XMLElement('li');
+			$li->setValue("Symphony CMS Version <strong>" . Administration::instance()->Configuration->get('version', 'symphony') . "</strong>");
+			$ul->prependChild($li);
+		}
+			
+	}
+
+?>
